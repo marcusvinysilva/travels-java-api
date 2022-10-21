cb47c97d1a3f41ad0fcf968e592137cf16b5151c
Marcus Silva <mvsilva1997@gmail.com>
2022-10-21 15:06:27 -0300

    feat: initial commit

---
+++ src/test/java/io/github/marcusvinysilva/travelsjavaapi/TravelsJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.marcusvinysilva.travelsjavaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelsJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
